7e9283802ad3605fd166892d41d379017c7727fa
박진아 <jin1090@greencatsoft.com>
2025-08-28 11:23:28 +0900

    next 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        esmExternals: false
+    },
+
+    webpack: (config) => {
+        config.externals.push({
+            'utf-8-validate': 'commonjs utf-8-validate',
+            'bufferutil': 'commonjs bufferutil',
+        })
+
+        return config
+    },
+    // 3D 파일 타입 허용
+    async headers() {
+        return [
+            {
+                source: '/models/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+        ]
+    },
 };
 
 export default nextConfig;
